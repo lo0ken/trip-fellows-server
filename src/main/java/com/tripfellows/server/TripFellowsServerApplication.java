32bfa0e56b70a34abd5feea230f30049afb4cdc7
Dmitriy <gavrilov.dmitriy04@gmail.com>
2020-09-28 23:24:41 +0300

    Initial commit

---
+++ src/main/java/com/tripfellows/server/TripFellowsServerApplication.java
@@ -0,0 +1,13 @@
+package com.tripfellows.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripFellowsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripFellowsServerApplication.class, args);
+	}
+
+}
