3b5243451f83b0c4e9ac84b1cc77d4340ebc5c0c
Danni <danilandroid2000@gmail.com>
2020-09-30 00:28:46 +0300

    Readme: add origin dev in git pull command

--- src/main/java/com/tripfellows/server/TripFellowsServerApplication.java
+++ src/main/java/com/tripfellows/server/TripFellowsServerApplication.java
@@ -8,8 +8,6 @@ public class TripFellowsServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TripFellowsServerApplication.class, args);
-
-		System.out.println("x");
 	}
 
 }
