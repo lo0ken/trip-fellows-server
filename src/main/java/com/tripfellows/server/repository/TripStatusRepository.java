4f275ecf8bfc81820464bdfdb218bca602559f44
Dmitriy <gavrilov.dmitriy04@gmail.com>
2020-10-01 17:56:17 +0300

    Repositories: add jpa repositories for entities

---
+++ src/main/java/com/tripfellows/server/repository/TripStatusRepository.java
@@ -0,0 +1,9 @@
+package com.tripfellows.server.repository;
+
+import com.tripfellows.server.entity.TripStatusEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TripStatusRepository extends JpaRepository<TripStatusEntity, Integer> {
+}
