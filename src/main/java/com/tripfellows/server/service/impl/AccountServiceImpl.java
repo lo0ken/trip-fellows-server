83f650988fddd97bbba72739cd741301a1cc8dab
Dmitriy <gavrilov.dmitriy04@gmail.com>
2020-10-01 21:42:14 +0300

    Mappers: mappers

--- src/main/java/com/tripfellows/server/service/impl/AccountServiceImpl.java
+++ src/main/java/com/tripfellows/server/service/impl/AccountServiceImpl.java
@@ -1,8 +1,10 @@
 package com.tripfellows.server.service.impl;
 
+import com.tripfellows.server.mapper.AccountMapper;
 import com.tripfellows.server.repository.AccountRepository;
 import com.tripfellows.server.service.api.AccountService;
 import lombok.extern.slf4j.Slf4j;
+import org.mapstruct.factory.Mappers;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,9 +17,12 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class AccountServiceImpl implements AccountService {
 
+    private final AccountMapper accountMapper;
+
     private final AccountRepository accountRepository;
 
     public AccountServiceImpl(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
+        accountMapper = Mappers.getMapper(AccountMapper.class);
     }
 }
--- src/main/java/com/tripfellows/server/service/impl/TripStatusServiceImpl.java
+++ src/main/java/com/tripfellows/server/service/impl/TripStatusServiceImpl.java
@@ -1,8 +1,10 @@
 package com.tripfellows.server.service.impl;
 
+import com.tripfellows.server.mapper.TripStatusMapper;
 import com.tripfellows.server.repository.TripStatusRepository;
 import com.tripfellows.server.service.api.TripStatusService;
 import lombok.extern.slf4j.Slf4j;
+import org.mapstruct.factory.Mappers;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,9 +17,12 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class TripStatusServiceImpl implements TripStatusService {
 
+    private final TripStatusMapper tripStatusMapper;
+
     private final TripStatusRepository tripStatusRepository;
 
     public TripStatusServiceImpl(TripStatusRepository tripStatusRepository) {
         this.tripStatusRepository = tripStatusRepository;
+        tripStatusMapper = Mappers.getMapper(TripStatusMapper.class);
     }
 }
