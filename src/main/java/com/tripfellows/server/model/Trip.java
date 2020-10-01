684dafedd5fd79e39129684a6e879b9520a40258
Lackier <47485336+lackier@users.noreply.github.com>
2020-10-01 21:13:16 +0300

    DTO: trip

---
+++ src/main/java/com/tripfellows/server/model/Trip.java
@@ -0,0 +1,26 @@
+package com.tripfellows.server.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class Trip {
+    private Integer id;
+    private Point startPoint;
+    private Point endPoint;
+    private BigDecimal price;
+    private Account creator;
+    private LocalDateTime createDate;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private String comment;
+    private TripStatus status;
+    private Integer placesCount;
+}
