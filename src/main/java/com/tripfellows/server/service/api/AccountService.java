7fd3b3233b9ae78f492c0d7b0cf106252e6ee501
Dmitriy <gavrilov.dmitriy04@gmail.com>
2020-10-03 16:05:58 +0300

    API: GET /api/accounts/{id} endpoint

--- src/main/java/com/tripfellows/server/service/api/AccountService.java
+++ src/main/java/com/tripfellows/server/service/api/AccountService.java
@@ -1,4 +1,15 @@
 package com.tripfellows.server.service.api;
 
+import com.tripfellows.server.model.Account;
+
+import java.util.Optional;
+
 public interface AccountService {
+
+    /**
+     * Retrieve account from database
+     * @param id account id to retrieve
+     * @return Optional with body the account or Optional.empty
+     */
+    Optional<Account> findById(Integer id);
 }
