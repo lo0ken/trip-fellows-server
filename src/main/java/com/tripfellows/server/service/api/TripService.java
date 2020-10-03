6519059d81774c8722421b870b255abf75c48aed
Lackier <47485336+lackier@users.noreply.github.com>
2020-10-03 19:55:58 +0300

    API: GET /api/trips/getByAccount/ endpoint

--- src/main/java/com/tripfellows/server/service/api/TripService.java
+++ src/main/java/com/tripfellows/server/service/api/TripService.java
@@ -2,6 +2,7 @@ package com.tripfellows.server.service.api;
 
 import com.tripfellows.server.model.Trip;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TripService {
@@ -13,4 +14,12 @@ public interface TripService {
      * @return Optional with body the trip or Optional.empty
      */
     Optional<Trip> findById(Integer id);
+
+    /**
+     * Retrieve trips from database
+     *
+     * @param accountId id of account which trips to retrieve
+     * @return trips of account or empty list
+     */
+    List<Trip> findTripsByAccount(Integer accountId);
 }
