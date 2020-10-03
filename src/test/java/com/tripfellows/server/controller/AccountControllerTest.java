5db28715a822ca12c8b48d0e48dfaf5fd0d76520
Dmitriy <gavrilov.dmitriy04@gmail.com>
2020-10-03 18:58:55 +0300

    API: POST /api/accounts endpoint

--- src/test/java/com/tripfellows/server/controller/AccountControllerTest.java
+++ src/test/java/com/tripfellows/server/controller/AccountControllerTest.java
@@ -1,5 +1,6 @@
 package com.tripfellows.server.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tripfellows.server.model.Account;
 import com.tripfellows.server.service.api.AccountService;
 import org.jeasy.random.EasyRandom;
@@ -54,6 +55,46 @@ public class AccountControllerTest {
 
         mockMvc.perform(get("/api/accounts/{id}", notExistsId))
                 .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$").doesNotExist());
+    }
+
+    @Test
+    public void createAccountTest() throws Exception {
+        EasyRandom easyRandom = new EasyRandom();
+        Account toCreate = easyRandom.nextObject(Account.class);
+        toCreate.setId(null);
+        Account created = new Account(toCreate.getName(), toCreate.getPhoneNumber());
+        created.setId(1);
+
+        when(accountService.save(toCreate)).thenReturn(created);
+
+        mockMvc.perform(post("/api/accounts")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsBytes(toCreate)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(created.getId()))
+                .andExpect(jsonPath("$.name").value(created.getName()))
+                .andExpect(jsonPath("$.phoneNumber").value(created.getPhoneNumber()));
+
+    }
+
+    @Test
+    public void createAccountWithExistingId() throws Exception {
+        Account account = new EasyRandom().nextObject(Account.class);
+
+        mockMvc.perform(post("/api/accounts")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsBytes(account)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").doesNotExist());
+    }
+
+    @Test
+    public void checkAccountIsRequired() throws Exception {
+        mockMvc.perform(post("/api/accounts")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsBytes(null)))
+                .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$").doesNotExist());
     }
 }
