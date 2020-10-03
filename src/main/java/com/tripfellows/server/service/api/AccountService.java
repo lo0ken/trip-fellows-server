f4c378da87cf030fa86897cbee9f2bc0a93615ec
Dmitriy <gavrilov.dmitriy04@gmail.com>
2020-10-03 18:58:55 +0300

    API: POST /api/accounts endpoint

--- src/main/java/com/tripfellows/server/service/api/AccountService.java
+++ src/main/java/com/tripfellows/server/service/api/AccountService.java
@@ -12,4 +12,13 @@ public interface AccountService {
      * @return Optional with body the account or Optional.empty
      */
     Optional<Account> findById(Integer id);
+
+    /**
+     * Update all information for a specific account, and return the modified account
+     * Or create it if not exists
+     *
+     * @param account account to create or update
+     * @return updated or created account
+     */
+    Account save(Account account);
 }
