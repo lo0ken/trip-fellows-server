75198c5a6ac1025a1ed1cd9e3aa83c9520a76eab
Dmitriy <gavrilov.dmitriy04@gmail.com>
2020-10-05 17:09:25 +0300

    API: Post /api/trips/addMember endpoint

--- src/test/java/com/tripfellows/server/service/api/TripAccountServiceTest.java
+++ src/test/java/com/tripfellows/server/service/api/TripAccountServiceTest.java
@@ -1,12 +1,17 @@
 package com.tripfellows.server.service.api;
 
 import com.tripfellows.server.entity.TripAccountEntity;
+import com.tripfellows.server.enums.RoleCodeEnum;
+import com.tripfellows.server.mapper.TripAccountMapper;
+import com.tripfellows.server.model.Role;
 import com.tripfellows.server.model.TripMember;
 import com.tripfellows.server.repository.TripAccountRepository;
+import com.tripfellows.server.service.impl.RoleServiceImpl;
 import com.tripfellows.server.service.impl.TripAccountServiceImpl;
 import org.jeasy.random.EasyRandom;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mapstruct.factory.Mappers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -17,6 +22,8 @@ import java.util.List;
 import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -27,6 +34,11 @@ public class TripAccountServiceTest {
 
     @InjectMocks
     TripAccountServiceImpl tripAccountService;
+
+    @Mock
+    RoleServiceImpl roleService;
+
+    TripAccountMapper tripAccountMapper = Mappers.getMapper(TripAccountMapper.class);
 
     @Test
     public void saveAllTest() {
@@ -50,5 +62,28 @@ public class TripAccountServiceTest {
         List<TripMember> result = tripAccountService.saveAll(1, new ArrayList<>());
 
         assertThat(result).hasSize(size);
+    }
+
+    @Test
+    public void addMemberTest() {
+        Integer tripId = 1;
+        Integer accountId = 2;
+        Role role = new Role(RoleCodeEnum.PASSENGER, "name");
+        role.setId(3);
+
+        TripAccountEntity tripAccountEntity = tripAccountMapper.from(tripId, accountId, role.getId());
+
+        when(roleService.findByCode(role.getCode())).thenReturn(role);
+        when(tripAccountRepository.save(any())).thenReturn(tripAccountEntity);
+
+        TripMember result = tripAccountService.addTripMember(tripId, accountId, RoleCodeEnum.PASSENGER);
+
+        verify(tripAccountRepository).save(argThat(t ->
+                t.getTrip().getId().equals(tripId) &&
+                t.getRole().getId().equals(role.getId()) &&
+                t.getAccount().getId().equals(accountId)));
+
+        assertThat(result.getRole().getId()).isEqualTo(role.getId());
+        assertThat(result.getAccount().getId()).isEqualTo(accountId);
     }
 }
