72994e79da1a044195cabca3494fd920b61bea19
Dmitriy <gavrilov.dmitriy04@gmail.com>
2020-11-30 18:01:18 +0300

    API: /api/trips/currentTrip endpoint

--- src/main/java/com/tripfellows/server/repository/TripRepository.java
+++ src/main/java/com/tripfellows/server/repository/TripRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TripRepository extends JpaRepository<TripEntity, Integer> {
@@ -16,4 +17,13 @@ public interface TripRepository extends JpaRepository<TripEntity, Integer> {
 
     @Query("select t from TripEntity t where t.status.id = (select ts.id from TripStatusEntity ts where ts.code = 'WAITING')")
     List<TripEntity> findAllActive();
+
+    @Query("select t from TripEntity t where t.creator.id = :accountId and" +
+            " t.status.id = (select ts.id from TripStatusEntity ts where ts.code = 'STARTED')" +
+            " or t.status.id = (select ts.id from TripStatusEntity ts where ts.code = 'WAITING')")
+    Optional<TripEntity> findCurrentDriverTrip(@Param("accountId") Integer accountId);
+
+
+    @Query("select ts from TripAccountEntity ts where ts.account.id = :accountId and ts.trip.status.id = (select ts.id from TripStatusEntity ts where ts.code = 'STARTED')")
+    Optional<TripEntity> findCurrentPassengerTrip(@Param("accountId") Integer accountId);
 }
