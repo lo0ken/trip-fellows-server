e317b89115e20390fee889e5cd3e0c5ce334e9b6
Daniel <chernov.daniel0@gmail.com>
2020-12-05 17:25:11 +0300

    bug-8: добавил server.error.include в проперти

--- src/main/java/com/tripfellows/server/exception/NoAvailablePlacesFoundException.java
+++ src/main/java/com/tripfellows/server/exception/NoAvailablePlacesFoundException.java
@@ -3,6 +3,7 @@ package com.tripfellows.server.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "No available places in the trip")
+@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR,
+        reason = "No available places in the trip")
 public class NoAvailablePlacesFoundException extends RuntimeException {
 }
