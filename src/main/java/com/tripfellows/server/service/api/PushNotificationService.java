791c2dfe59b17b9f353438f4783cda37362a8253
Dmitriy <gavrilov.dmitriy04@gmail.com>
2020-12-20 20:33:45 +0300

    Pushes: notify trip created

---
+++ src/main/java/com/tripfellows/server/service/api/PushNotificationService.java
@@ -0,0 +1,21 @@
+package com.tripfellows.server.service.api;
+
+import com.tripfellows.server.model.Trip;
+
+public interface PushNotificationService {
+
+    /**
+     * Notifying all subscribed users that trip has been created
+     *
+     * @param trip created trip
+     */
+    void notifyTripCreated(Trip trip);
+
+
+    /**
+     * Notifying all passengers of the trip that trip status has been changed
+     *
+     * @param trip trip with new status
+     */
+    void notifyTripStatusChanged(Trip trip);
+}
